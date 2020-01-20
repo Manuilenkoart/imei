e6d98be0d05de6ce8c3d2113dd4b9f114ad920e4
Manuilenkoart <manuilenkoart@gmail.com>
2020-01-20 11:43:14 +0200

    Updates

---
+++ precache-manifest.251880a892038c55a7a8bd5823ef4d19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f52f07d1666c4693ebb144a6e1fbeedd",
+    "url": "/imei/index.html"
+  },
+  {
+    "revision": "a38be0aa2aeae0d36b80",
+    "url": "/imei/static/css/main.d1bc204a.chunk.css"
+  },
+  {
+    "revision": "59bc34e762d6e38b2930",
+    "url": "/imei/static/js/2.fc69f566.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/imei/static/js/2.fc69f566.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a38be0aa2aeae0d36b80",
+    "url": "/imei/static/js/main.b771d99e.chunk.js"
+  },
+  {
+    "revision": "57e6319e38cde1b744c4",
+    "url": "/imei/static/js/runtime-main.67e0d04d.js"
+  }
+]);
